8abade1cf70106504eebfa61cdb985b7a131a30a
Bhanjo <hanjo1515@naver.com>
2021-08-24 16:37:24 +0900

    추가: static 이미지 업로드 방법 학습

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,21 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function FirstPost() {
     return (
     <>
-        <h1>첫 번쨰 포스팅!</h1>
+        <h1>첫 번째 포스팅!</h1>
         <h2>
             <Link href="/">
                 <a>홈으로</a>
             </Link>
         </h2>
+        <Image
+            src='/images/profile.jpg'
+            height={144}
+            width={144}
+            alt='나의 이미지'
+        />
     </>
     )
 }
