1eb0c00a65bd230f7361e1423a205bc91f31ada4
Bhanjo <hanjo1515@naver.com>
2021-08-24 15:50:43 +0900

    추가: link 컴포넌트 기능 학습

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function FirstPost() {
-    return <h1>첫 번째 포스팅!</h1>
+    return (
+    <>
+        <h1>첫 번쨰 포스팅!</h1>
+        <h2>
+            <Link href="/">
+                <a>홈으로</a>
+            </Link>
+        </h2>
+    </>
+    )
 }
