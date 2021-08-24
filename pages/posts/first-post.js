7ff2ca53fbe7e9a011f42a81af4c41fe05065d82
Bhanjo <hanjo1515@naver.com>
2021-08-24 16:39:01 +0900

    추가: head 사용 방법 학습

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import Head from 'next/head'
 
 export default function FirstPost() {
     return (
     <>
+        <Head>
+            <title>첫 포스팅</title>
+        </Head>
         <h1>첫 번째 포스팅!</h1>
         <h2>
             <Link href="/">
